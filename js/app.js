b0951171eac59406778da36f144fef81e40e10df
Jacob Devin Patterson <jedijacob11@gmail.com>
2018-06-11 22:25:34 -0500

    Add player class, add movements for bugs

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x , y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = x;
+    this.y = y;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -13,24 +14,50 @@ var Enemy = function() {
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
-    // all computers.
+    // all computers
+        this.x = this.x+(Math.random() * 10 *dt*35);
 };
-
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+}
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
+class Player {
+    constructor(x = 0, y = 0) {
+        this.x = x;
+        this.y = y;
+        this.boy = 'images/char-boy.png';
+        function render() {
+            ctx.drawImage(Resources.get(this.boy), this.x ,this.y);
+        }
+    }
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
 
+let allEnemies = [];
+let newEnemies = setInterval(function () {
+allEnemies.push(new Enemy(0,62));
+allEnemies.push(new Enemy(0,145));
+allEnemies.push(new Enemy(0,227));
+},1000);
+
+let player = [];
+player.push(new Player(100,100));
+
+player.handleInput = function (){
+}
+
+player.update = function(dt) {
+}
+player.render = function() {
+}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
