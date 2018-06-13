9a9f37e08283626becb954611452c0369c80005f
Jacob Devin Patterson <jedijacob11@gmail.com>
2018-06-12 22:22:28 -0500

    Create player image, create movement, create movement perameters

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 var Enemy = function(x , y) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-    this.x = x;
+    this.x = x
     this.y = y;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
@@ -15,7 +15,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers
-        this.x = this.x+(Math.random() * 10 *dt*35);
+    this.x = this.x+(Math.random()  * 10 *dt*35);
 };
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
@@ -30,9 +30,6 @@ class Player {
         this.x = x;
         this.y = y;
         this.boy = 'images/char-boy.png';
-        function render() {
-            ctx.drawImage(Resources.get(this.boy), this.x ,this.y);
-        }
     }
 }
 
@@ -40,23 +37,56 @@ class Player {
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
 let allEnemies = [];
-let newEnemies = setInterval(function () {
+allEnemies[Math.floor(Math.random()*allEnemies.length)]
+setInterval(function () {
 allEnemies.push(new Enemy(0,62));
 allEnemies.push(new Enemy(0,145));
 allEnemies.push(new Enemy(0,227));
 },1000);
 
-let player = [];
-player.push(new Player(100,100));
 
-player.handleInput = function (){
+
+
+allEnemies.forEach(function() {
+
+});
+
+console.log(allEnemies)
+
+let player = new Player(200,300);
+console.log(player)
+
+player.handleInput = function (y){
+    player.update(y);
+    if(this.x === 500){
+        this.x = 400;
+    } if(this.x === -100) {
+        this.x = 0;
+    } if (this.y === 470) {
+        this.y = 385;
+    } else if (this.y === -40) {
+        this.x = 200;
+        this.y = 300;
+    }
 }
 
-player.update = function(dt) {
+player.update = function(y) {
+    if (y === 'left'){
+        this.x -= 100;
+    } if (y === 'right'){
+        this.x += 100;
+    } if (y === 'up'){
+        this.y -= 85;
+    } if (y === 'down'){
+        this.y += 85;
+    } else {
+        this.x += 0;
+        this.y += 0;
+    }
 }
 player.render = function() {
+    ctx.drawImage(Resources.get(this.boy),this.x,this.y);
 }
 
 // This listens for key presses and sends the keys to your
